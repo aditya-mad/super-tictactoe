193159c98591d5fcd06858823436c3632577ef20
aditya-mad <maddulapalliaditya@gmail.com>
2023-11-04 19:26:58 +0530

    game-flow -l

--- main.cpp
+++ main.cpp
@@ -12,17 +12,21 @@ const char INTIAL_FILL = '1';
 class TicTacToe
 {
 private:
-    int current_block;
+    int current_block, move;
+    char current_win, current_move;
+    bool is_game_done = false;
     vector<vector<vector<char>>> board;
     vector<vector<char>> board_score;
 
-    bool validateBoard(const vector<vector<char>> &);
-    bool isBoardEmpty(const vector<vector<char>> &);
-    bool isMoveValid(int);
+    void startGame();
     void printBoard();
     void printRow(int, int, int);
+    bool isBoardEmpty(const vector<vector<char>> &);
+    bool isMoveValid(int);
     void setCurrentBlock();
     void makeMove(char, int);
+    void chooseMove();
+    bool validateBoard(const vector<vector<char>> &);
 
 public:
     TicTacToe();
@@ -38,7 +42,29 @@ TicTacToe::TicTacToe()
 {
     board.resize(SIZE * SIZE, vector<vector<char>>(SIZE, vector<char>(SIZE, INTIAL_FILL)));
     board_score.resize(SIZE, vector<char>(SIZE, ' '));
-    printBoard();
+    startGame();
+}
+
+void TicTacToe::startGame()
+{
+    cin >> current_block;
+    for (int i = 0; i < 81; i++)
+    {
+        current_move = (i % 2 == 0) ? 'X' : 'O';
+
+        CLEAR_SCREEN;
+        // if (validateBoard(board_score))
+        // {
+        //     cout << current_win << " Won the game\n";
+        //     return;
+        // }
+
+        printBoard();
+        setCurrentBlock();
+        chooseMove();
+        makeMove(current_move, move);
+        validateBoard(board[current_block]);
+    }
 }
 
 void TicTacToe::printBoard()
@@ -87,8 +113,10 @@ bool TicTacToe::isMoveValid(int cell)
 
 void TicTacToe::setCurrentBlock()
 {
-    if (isBoardEmpty(board[current_block]))
+    if (!isBoardEmpty(board[current_block]))
         return;
+
+    cout << "Choose Block - ";
     cin >> current_block;
 }
 
@@ -96,6 +124,12 @@ void TicTacToe::makeMove(char player, int cell)
 {
     if (isMoveValid(cell))
         board[current_block][cell / 3][cell % 3] = player;
+}
+
+void TicTacToe::chooseMove()
+{
+    cout << "Cell - ";
+    cin >> move;
 }
 
 bool TicTacToe::validateBoard(const vector<vector<char>> &grid)
@@ -106,8 +140,10 @@ bool TicTacToe::validateBoard(const vector<vector<char>> &grid)
     {
         int count = 0;
         for (int j = 0; j < SIZE - 1; j++)
+        {
             count += 1 * (grid[i][j] == grid[i][j + 1]);
-
+            current_win = grid[i][j];
+        }
         line = (count == 2);
 
         if (line)
@@ -118,8 +154,10 @@ bool TicTacToe::validateBoard(const vector<vector<char>> &grid)
     {
         int count = 0;
         for (int j = 0; j < SIZE; j++)
+        {
             count += 1 * (grid[j][i] == grid[j][i + 1]);
-
+            current_win = grid[i][j];
+        }
         line = (count == 2);
 
         if (line)
@@ -129,13 +167,16 @@ bool TicTacToe::validateBoard(const vector<vector<char>> &grid)
     for (int i = 0; i < SIZE - 1; i++)
     {
         line = (grid[i][i] == grid[i + 1][i + 1]);
+        current_win = grid[i][i];
     }
 
     if (line)
         return line;
 
     for (int i = SIZE - 1; i > 0; i--)
+    {
         line = (grid[i][SIZE - i - 1] == grid[i - 1][SIZE - i]);
-
+        current_win = grid[i][SIZE - i - 1];
+    }
     return line;
 }
